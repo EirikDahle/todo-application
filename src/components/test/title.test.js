72e50cf772a83404084141ad6ff91925f97c598b
Eirik <eiriknd@stud.ntnu.no>
2022-11-17 14:44:48 +0100

    Test should work

---
+++ src/components/test/title.test.js
@@ -0,0 +1,10 @@
+import {screen, cleanup, render} from "@testing-library/react"
+import '@testing-library/jest-dom'
+import App from '../../App' 
+
+test("navbar displays correct", () => {
+    render(<App />)
+    const title = screen.getByTestId("title");
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent("Post-it todos")
+});
