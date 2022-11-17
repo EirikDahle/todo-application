89b21e00563f9a3450b91a6ae1dcdaf7958d99ef
Eirik <eiriknd@stud.ntnu.no>
2022-11-17 14:54:30 +0100

    Added passing test to check if pipeline deploys

--- src/components/test/title.test.js
+++ src/components/test/title.test.js
@@ -1,6 +1,6 @@
 import {screen, cleanup, render} from "@testing-library/react"
 import '@testing-library/jest-dom'
-import App from '../../App' 
+import App from '../../App.js' 
 
 test("navbar displays correct", () => {
     render(<App />)
